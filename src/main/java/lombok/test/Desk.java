396c418c7ed98970df17f07d54bcfd9e4487b290
jaehyun-Yu <yjh454@naver.com>
2021-06-14 16:28:01 +0900

    20210614

---
+++ src/main/java/lombok/test/Desk.java
@@ -0,0 +1,10 @@
+package lombok.test;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Desk {
+	private final double price;
+	private final String model;
+	private String name;
+}
